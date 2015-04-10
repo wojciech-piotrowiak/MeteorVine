d4c193c350cf3765b13830d3a58e51d703e53ee5
wojciech-piotrowiak <wojciech.piotrowiak@gmail.com>
2015-04-10 22:52:58 +0200

    Routing improved
    Features separated in menu

--- meteorVine/routing.js
+++ meteorVine/routing.js
@@ -1,3 +1,16 @@
-Router.route('/');
-Router.route('/contact');
-Router.route('/recipies');
+ Router.configure({
+  layoutTemplate: 'bodyTemplate'
+});
+
+Router.route('/', function () {
+ this.render('allVines', {to: 'internal'});
+ } );
+ 
+Router.route('/contact', function () {
+ this.render('contact', {to: 'internal'});
+ } );
+ 
+Router.route('/recipies', function () {
+ this.render('recipies', {to: 'internal'});
+ } );
+ 
